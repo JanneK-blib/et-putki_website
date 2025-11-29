1a64ac3f99958802aa90d01889520027bccc75b1
Jan Kokkonen <jan.kokkonen@prog-it.net>
2025-11-29 18:06:05 +0200

    Created pricing section

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -34,10 +34,7 @@ window.addEventListener('DOMContentLoaded', () => {
 document.querySelector('.hamburger-icon').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.nav-menu').classList.toggle('active');
-    const middleSection = document.querySelector('.middle-section');
-    if (middleSection) {
-      middleSection.classList.toggle('hiddenText');
-    }
+    document.querySelector('.middle-section').classList.toggle('hiddenText');
 });
 
 document.querySelectorAll('.nav-link a').forEach(function(link) {
