c1857f6b222ed2a5d5d5b0da3040569f28e6c767
Jan Kokkonen <jan.kokkonen@prog-it.net>
2025-11-29 17:48:08 +0200

    fixed .middle section error

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -34,7 +34,10 @@ window.addEventListener('DOMContentLoaded', () => {
 document.querySelector('.hamburger-icon').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.nav-menu').classList.toggle('active');
-    document.querySelector('.middle-section').classList.toggle('hiddenText');
+    const middleSection = document.querySelector('.middle-section');
+    if (middleSection) {
+      middleSection.classList.toggle('hiddenText');
+    }
 });
 
 document.querySelectorAll('.nav-link a').forEach(function(link) {
