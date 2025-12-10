d73b0c15a50211a6966d5fe868b16e7595b150b8
Jan Kokkonen <jan.kokkonen@prog-it.net>
2025-12-10 17:10:27 +0200

    move emailjs  Public Key, Service ID

---
+++ config/emailjs-config.js
@@ -0,0 +1,15 @@
+/**
+ * EmailJS Configuration
+ * IMPORTANT: This file should NOT be committed to version control!
+ * Add this file to .gitignore
+ */
+
+const EMAILJS_CONFIG = {
+    publicKey: 'yoW4TRKQG3tzOcdQ9',
+    serviceId: 'service_yzdjwob',
+    templateId: 'template_4vxbn27',
+    toEmail: 'info.etputki@gmail.com'
+};
+
+// Make config available globally
+window.EMAILJS_CONFIG = EMAILJS_CONFIG;
